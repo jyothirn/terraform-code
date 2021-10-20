8ef3a9aa7ffcfa81ca1056aa77204f2a3a1e89e7
jyothi <jyothi.cse14@gmail.com>
2021-10-20 13:49:14 +0530

    first commit

---
+++ instance.tf
@@ -0,0 +1,11 @@
+provide "aws" {
+ acces_key = "AKIAYJA7WUTDEMRZQNKX"
+ secret_key = "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"
+ region = "us-east-1"
+
+}
+resource "aws_instance" "te-demo"{
+ ami = "ami-02e136e904f3da870"
+ instance_type = "t2.micro"
+}
+
